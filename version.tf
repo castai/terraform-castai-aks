7f4b29d023834f82f007613fb52cc550a1ac7cf6
Sam Gibson <sgibson@phlexglobal.com>
2022-07-29 11:58:39 +0100

    Make azurerm/azuread version constraints less restrictive (fixes #9)

--- version.tf
+++ version.tf
@@ -3,12 +3,12 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.7.0"
+      source  = "hashicorp/azurerm"
+      version = ">=3.7.0"
     }
     azuread = {
-      source = "hashicorp/azuread"
-      version = "2.22.0"
+      source  = "hashicorp/azuread"
+      version = ">=2.22.0"
     }
     castai = {
       source  = "castai/castai"
