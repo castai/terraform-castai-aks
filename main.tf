fb637dac7dffcae44157906b1c75978eccab6d51
linkas45 <mataskam@gmail.com>
2024-05-13 17:37:20 +0700

    update

--- main.tf
+++ main.tf
@@ -84,8 +84,6 @@ resource "castai_node_template" "this" {
     content {
       compute_optimized                           = try(constraints.value.compute_optimized, null)
       storage_optimized                           = try(constraints.value.storage_optimized, null)
-      compute_optimized_status                    = try(constraints.value.compute_optimized_status, "")
-      storage_optimized_status                    = try(constraints.value.storage_optimized_status, "")
       spot                                        = try(constraints.value.spot, false)
       on_demand                                   = try(constraints.value.on_demand, null)
       use_spot_fallbacks                          = try(constraints.value.use_spot_fallbacks, false)
