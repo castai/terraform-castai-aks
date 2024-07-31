8a83cc240a492707fe9b8b2dae14243eeaa478af
Matas Kaminskas <mataskam@gmail.com>
2024-07-31 11:59:11 +0530

    Customer specific instances (#77)
    * impl
    
    * rename
    
    * version

--- main.tf
+++ main.tf
@@ -103,6 +103,7 @@ resource "castai_node_template" "this" {
       os                                          = try(constraints.value.os, ["linux"])
       azs                                         = try(constraints.value.azs, null)
       burstable_instances                         = try(constraints.value.burstable_instances, null)
+      customer_specific                           = try(constraints.value.customer_specific, null)
 
       dynamic "instance_families" {
         for_each = [for instance_families in flatten([lookup(constraints.value, "instance_families", [])]) : instance_families if instance_families != null]
