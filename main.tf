3d17999af680cb994f5b24602107a62c1031402c
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2024-10-10 12:58:35 +0200

    conflict fix

--- main.tf
+++ main.tf
@@ -46,6 +46,20 @@ resource "castai_node_configuration" "this" {
     max_pods_per_node = try(each.value.max_pods_per_node, 30)
     os_disk_type      = try(each.value.os_disk_type, null)
     aks_image_family  = try(each.value.aks_image_family, null)
+    dynamic "loadbalancers" {
+      for_each = flatten([lookup(each.value, "loadbalancers", [])])
+
+      content {
+        name = try(loadbalancers.value.name, null)
+        dynamic "ip_based_backend_pools" {
+          for_each = flatten([lookup(loadbalancers.value, "ip_based_backend_pools", [])])
+
+          content {
+            name = try(ip_based_backend_pools.value.name, null)
+          }
+        }
+      }
+    }
   }
 }
 
