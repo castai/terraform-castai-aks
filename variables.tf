348e76454f8e5321c2932ade88bd65aec8a64311
Andrej Kislovskij <andrej@cast.ai>
2025-12-05 14:38:43 +0200

    Add Omni support

--- variables.tf
+++ variables.tf
@@ -310,3 +310,9 @@ variable "ai_optimizer_values" {
   type        = list(string)
   default     = []
 }
+
+variable "install_omni" {
+  description = "Optional flag for installing Omni capability"
+  type        = bool
+  default     = false
+}
