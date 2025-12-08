1e22a98e679f8f1fe90c8848b7ffb4dd38e4faf0
Primož Hrovat <primoz@cast.ai>
2025-12-08 08:08:41 +0100

    chore: remove usage of deprecated autoscaler policies settings (#120)
    * chore: remove usage of deprecated autoscaler policies settings
    
    * chore: regenerate docs
    
    ---------
    
    Co-authored-by: Daniel Voros <daniel.voros@cast.ai>

--- variables.tf
+++ variables.tf
@@ -48,12 +48,6 @@ variable "subscription_id" {
   description = "Azure subscription ID"
 }
 
-variable "autoscaler_policies_json" {
-  type        = string
-  description = "Optional json object to override CAST AI cluster autoscaler policies. Deprecated, use `autoscaler_settings` instead."
-  default     = null
-}
-
 variable "autoscaler_settings" {
   type        = any
   description = "Optional Autoscaler policy definitions to override current autoscaler settings"
