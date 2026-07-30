23431984c1f908e3ded5839461828fe71c56c56d
Daniel Voros <daniel.voros@cast.ai>
2026-07-30 10:27:32 +0200

    chore: don't allow azurerm 5.0.0 yet (#145)

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      version = ">= 3.7.0, < 5.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3"
+      version = "~> 3.0"
     }
     castai = {
       source  = "castai/castai"
